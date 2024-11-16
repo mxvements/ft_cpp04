a312e7bed03f0d1b78c3c48f27745bfb4aa03e9d
luciama2 <luciama2@student.42madrid.com>
2024-11-16 14:09:03 +0100

    ex03, fix on Character class

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -6,20 +6,26 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:52:31 by luciama2          #+#    #+#             */
-/*   Updated: 2024/11/15 22:21:00 by luciama2         ###   ########.fr       */
+/*   Updated: 2024/11/16 14:05:03 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Character.hpp"
 #include "Colors.hpp"
 
+/**
+ * Materias must be deleted when a Character is destroyed
+ */
 Character::~Character(void)
 {
-	// for (int i = 0; i < 4; i++)
-	// {
-	// 	if (this->_inventory[i])
-	// 		delete (this->_inventory[i]);
-	// }
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_inventory[i])
+		{
+			delete (this->_inventory[i]);
+			this->_inventory[i] = NULL;
+		}
+	}
 	std::cout << YLLW << "Character destructor called" << RESET << std::endl;
 }
 
@@ -41,27 +47,30 @@ Character::Character(const std::string name) : _name(name)
 	}
 }
 
-Character::Character(const Character &src)
+Character::Character(const Character &src) : _name(src.getName())
 {
 	std::cout << YLLW << "Character copy constructor called" << RESET << std::endl;
+	for (int i = 0; i < 4; i++)
+		this->_inventory[i] = NULL;
 	*this = src;
-	// for (int i = 0; i < 4; i++)
-	// {
-	// 	this->_inventory[i] = src._inventory[i];
-	// }
 }
 
 Character &Character::operator=(const Character &src)
 {
 	std::cout << YLLW << "Character assignment overload called" << RESET << std::endl;
 	this->_name = src._name;
-	this->setInventory(src._inventory); //BUUUUUG
-	// for (int i = 0; src._inventory[i]; i++)
-	// {
-	// 	if (this->_inventory[i])
-	// 		delete this->_inventory[i];
-	// 	this->_inventory[i] = src._inventory[i];
-	// }
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_inventory[i])
+		{
+			delete this->_inventory[i];
+			this->_inventory[i] = NULL;
+		}
+		if (!src._inventory[i])
+			continue;
+		this->_inventory[i] = src._inventory[i]->clone();
+	}
 	return (*this);
 }
 
@@ -71,7 +80,9 @@ void Character::setInventory(AMateria *const inventory[4])
 	{
 		if (this->_inventory[i])
 			delete this->_inventory[i];
-		this->_inventory[i] = inventory[i];
+		if (!inventory[i])
+			continue ; //This allows to pput an inventory with an empty slot
+		this->_inventory[i] = inventory[i]->clone();
 	}
 }
 
@@ -96,16 +107,14 @@ void Character::equip(AMateria *m)
 {
 	for (int i = 0; i < 4; i++)
 	{
-		if (this->_inventory[i] != NULL)
+		if (!this->_inventory[i])
 		{
-			if (i == 3)
-				std::cout << "Full inventory" << std::endl;
-			continue;
+			this->_inventory[i] = m->clone();
+			return ;
 		}
-		this->_inventory[i] = m;
-		break;
 	}
-	return;
+	std::cout << "Full inventory" << std::endl;
+	return ;
 }
 /**
  * unequip does not delete the Materia
@@ -118,13 +127,14 @@ void Character::unequip(int idx)
 {
 	if (!this->_inventory[idx])
 	{
-		std::cout << "Amateria at idx " << idx << " is null, cannot unequip"  << std::endl;
-		return ;
+		std::cout << "Amateria at idx " << idx << " is null, cannot unequip" << std::endl;
+		return;
 	}
 	for (int i = idx; (i + 1) < 4; i++)
 	{
 		this->_inventory[i] = this->_inventory[i + 1];
 	}
+	this->_inventory[3] = NULL;
 }
 /**
  * use the materia at slot[idx] and pass the target parameter to the AMaterria::use
@@ -136,7 +146,7 @@ void Character::use(int idx, ICharacter &target)
 	if (!m)
 	{
 		std::cout << "Amateria at idx " << idx << " is null, cannot use" << std::endl;
-		return ;
+		return;
 	}
 	m->use(target);
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 21:07:46 by luciama2          #+#    #+#             */
-/*   Updated: 2024/11/15 22:19:41 by luciama2         ###   ########.fr       */
+/*   Updated: 2024/11/16 14:08:24 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,63 +55,78 @@ int main(void)
 	// 	std::cout << std::endl;
 	// }
 	{
-		//test of Character class, mostly equip and unequip functs
+		// test of Character class, mostly equip and unequip functs
+		std::cout << std::endl;
+		std::cout << "/****** Character ******/" << std::endl;
 		Character c("paco");
 		Ice *ice1 = new Ice();
 		Ice *ice2 = new Ice();
 		Cure *cure1 = new Cure();
 		Cure *cure2 = new Cure();
 		Cure *cure3 = new Cure();
-		
-		//Character has 4 slots for materias
-		c.equip(ice1);
-		c.equip(cure1);
-		
-		c.use(3, c); //check for null amateria
-		c.unequip(3); //check for null materia
 
-		c.equip(ice2);
-		c.equip(cure2);
-		c.equip(cure3); //extra materia, cannot equip
-		
-		c.use(2, c);
-		c.unequip(2); //unequips ann ice, next is cure
-		c.use(2, c);
-	
-		c.unequip(0);
-		c.unequip(1);
-		c.unequip(2);
-		c.unequip(3);
+		// Character has 4 slots for materias
+		c.equip(ice1); //0
+		c.equip(cure1); //1
 
-		//check deep copy of Character
+		c.use(3, c);  // check for null amateria
+		c.unequip(3); // check for null materia
+
+		c.equip(ice2); //2
+		c.equip(cure2); //3
+		c.equip(cure3); //4 -  extra materia, cannot equip
+
+		c.use(2, c);
+		AMateria *unequiped = c.getInventory()[2];
+		c.unequip(2); // unequips ann ice, next is cure
+		delete unequiped;
+		c.use(2, c); //nest amateria
+
+		// check deep copy of Character
 		Character c_cp(c);
-		std::cout << "c address: " << &c << std::endl;
-		std::cout << "cp addres: " << &c_cp << std::endl;
-		std::cout << "c inventory addr: " << c.getInventory() << std::endl;
-		std::cout << "cp inventory addr: "<< c_cp.getInventory() << std::endl;
+		std::cout << "c address: \t" << &c << std::endl;
+		std::cout << "cp addres: \t" << &c_cp << std::endl;
+		std::cout << "c inventory addr: \t" << c.getInventory() << std::endl;
+		std::cout << "cp inventory addr: \t" << c_cp.getInventory() << std::endl;
+		for (int i = 0; i < 4; i++)
+		{
+			std::cout << "inventory item of c, \tidx: " << i << " w/address:\t" << c.getInventory()[i]<< std::endl;
+			std::cout << "inventory item, of cpy \tidx: " << i << " w/address:\t" << c_cp.getInventory()[i]<< std::endl;
+		}
 
+		delete ice2; //unequiped
+		delete cure3; //never equiped
 		delete ice1;
-		delete ice2;
 		delete cure1;
 		delete cure2;
-		delete cure3;
+	}
+	{
+		// Check for MateriaSource class
+		std::cout << std::endl;
+		std::cout << "/****** Materia Source ******/" << std::endl;
 	}
 
-	// IMateriaSource *src = new MateriaSource();
-	// src->learnMateria(new Ice());
-	// src->learnMateria(new Cure());
-	// ICharacter *me = new Character("me");
-	// AMateria *tmp;
-	// tmp = src->createMateria("ice");
-	// me->equip(tmp);
-	// tmp = src->createMateria("cure");
-	// me->equip(tmp);
-	// ICharacter *bob = new Character("bob");
-	// me->use(0, *bob);
-	// me->use(1, *bob);
-	// delete bob;
-	// delete me;
-	// delete src;
+	// {
+	// 	// Check for MateriaSource class
+	// 	std::cout << std::endl;
+	// 	std::cout << "/****** Provided main ******/" << std::endl;
+		
+	// 	IMateriaSource *src = new MateriaSource();
+	// 	src->learnMateria(new Ice());
+	// 	src->learnMateria(new Cure());
+	// 	ICharacter *me = new Character("me");
+	// 	AMateria *tmp;
+	// 	tmp = src->createMateria("ice");
+	// 	me->equip(tmp);
+	// 	tmp = src->createMateria("cure");
+	// 	me->equip(tmp);
+	// 	ICharacter *bob = new Character("bob");
+	// 	me->use(0, *bob);
+	// 	me->use(1, *bob);
+	// 	delete bob;
+	// 	delete me;
+	// 	delete src;
+	// }
 
 	return (0);
 }
