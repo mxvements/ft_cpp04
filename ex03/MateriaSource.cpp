f68766bab56f73f27051ad186cbe591bf26031bc
luciama2 <luciama2@student.42madrid.com>
2024-11-16 16:10:54 +0100

    fix ex03 MateriaSource

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 19:46:13 by luciama2          #+#    #+#             */
-/*   Updated: 2024/10/26 20:30:01 by luciama2         ###   ########.fr       */
+/*   Updated: 2024/11/16 16:13:36 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@
 
 MateriaSource::~MateriaSource(void)
 {
-	for (int i = 0; i < 4; i++)
-	{
-		delete (this->_known_materias[i]);
-	}
 	std::cout << ORANGE << "MateriaSource destructor called" << RESET << std::endl;
 }
 
@@ -35,13 +31,22 @@ MateriaSource::MateriaSource(void)
 MateriaSource::MateriaSource(const MateriaSource &src)
 {
 	std::cout << ORANGE << "MateriaSource copy constructor called" << RESET << std::endl;
+	for (int i = 0; i < 4; i++)
+		this->_known_materias[i] = NULL;
 	*this = src;
 }
 
 MateriaSource &MateriaSource::operator=(const MateriaSource &src)
 {
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_known_materias[i])
+			this->_known_materias[i] = NULL;
+		if (!src._known_materias[i])
+			continue;
+		this->_known_materias[i] = src._known_materias[i]; 
+	}
 	std::cout << ORANGE << "MateriaSource assignment overload called" << RESET << std::endl;
-	this->setKnownMaterias(src._known_materias);
 	return (*this);
 }
 
@@ -50,8 +55,10 @@ void MateriaSource::setKnownMaterias(AMateria *const known_materias[4])
 	for (int i = 0; i < 4; i++)
 	{
 		if (this->_known_materias[i])
-			delete (this->_known_materias[i]);
-		this->_known_materias[i] = known_materias[i];
+			this->_known_materias[i] = NULL;
+		if (!known_materias[i])
+			continue;
+		this->_known_materias[i] = known_materias[i]; 
 	}
 	return;
 }
@@ -62,7 +69,7 @@ AMateria **MateriaSource::getKnownMaterias(void)
 }
 
 /**
- * copies the materia passed as a parameter and store it in memory so it can be clones
+ * copies the materia passed as a parameter and store it in memory so it can be cloned
  * later. Like the character, the materiasource, can know at most 4 materias
  * They are not necessarily unique
  */
@@ -70,16 +77,15 @@ void MateriaSource::learnMateria(AMateria *m)
 {
 	for (int i = 0; i < 4; i++)
 	{
-		if (this->_known_materias[i])
+		if (!this->_known_materias[i])
 		{
-			if (i == 3)
-				std::cout << "Full known materias" << std::endl;
-			continue;
+			std::cout << "adding materia at idx: " << i << std::endl;
+			this->_known_materias[i] = m;
+			return ;
 		}
-		this->_known_materias[i] = m;
-		break;
 	}
-	return;
+	std::cout << "Full known materias catalog!" << std::endl;
+	return ;
 }
 
 /**
@@ -92,7 +98,7 @@ AMateria *MateriaSource::createMateria(std::string const &type)
 	for (int i = 0; i < 4; i++)
 	{
 		if (this->_known_materias[i] && this->_known_materias[i]->getType().compare(type) == 0)
-			return ((this->_known_materias[i])->clone());
+			return ((this->_known_materias[i]));
 	}
-	return (NULL);
+	return (0);
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 21:07:46 by luciama2          #+#    #+#             */
-/*   Updated: 2024/11/16 14:08:24 by luciama2         ###   ########.fr       */
+/*   Updated: 2024/11/16 16:14:44 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ int main(void)
 	// 	ice_clone->use(target);
 	// 	std::cout << std::endl;
 	// }
+	
 	{
 		// test of Character class, mostly equip and unequip functs
 		std::cout << std::endl;
@@ -64,24 +65,19 @@ int main(void)
 		Cure *cure1 = new Cure();
 		Cure *cure2 = new Cure();
 		Cure *cure3 = new Cure();
-
 		// Character has 4 slots for materias
 		c.equip(ice1); //0
 		c.equip(cure1); //1
-
 		c.use(3, c);  // check for null amateria
 		c.unequip(3); // check for null materia
-
 		c.equip(ice2); //2
 		c.equip(cure2); //3
 		c.equip(cure3); //4 -  extra materia, cannot equip
-
 		c.use(2, c);
 		AMateria *unequiped = c.getInventory()[2];
 		c.unequip(2); // unequips ann ice, next is cure
 		delete unequiped;
 		c.use(2, c); //nest amateria
-
 		// check deep copy of Character
 		Character c_cp(c);
 		std::cout << "c address: \t" << &c << std::endl;
@@ -93,40 +89,65 @@ int main(void)
 			std::cout << "inventory item of c, \tidx: " << i << " w/address:\t" << c.getInventory()[i]<< std::endl;
 			std::cout << "inventory item, of cpy \tidx: " << i << " w/address:\t" << c_cp.getInventory()[i]<< std::endl;
 		}
-
 		delete ice2; //unequiped
 		delete cure3; //never equiped
 		delete ice1;
 		delete cure1;
 		delete cure2;
 	}
+	
 	{
 		// Check for MateriaSource class
 		std::cout << std::endl;
 		std::cout << "/****** Materia Source ******/" << std::endl;
+		MateriaSource mt;
+		Ice *ice = new Ice();
+		Cure *cure = new Cure();
+		mt.learnMateria(ice);
+		mt.learnMateria(cure);
+		AMateria *lm_ice = mt.createMateria("ice"); //creates from knwon materias
+		std::cout << lm_ice->getType() << std::endl;
+		AMateria *lm_cure = mt.createMateria("cure");
+		std::cout << lm_cure->getType() << std::endl;
+		mt.learnMateria(lm_ice);
+		mt.learnMateria(lm_cure);
+		mt.learnMateria(ice); //Full learn materias
+		//Check deep copying
+		MateriaSource mt_cp(mt);
+		std::cout << "mt address: \t" << &mt << std::endl;
+		std::cout << "mt_cp addres: \t" << &mt_cp << std::endl;
+		std::cout << "mt inventory addr: \t" << mt.getKnownMaterias() << std::endl;
+		std::cout << "mt_cp inventory addr: \t" << mt_cp.getKnownMaterias() << std::endl;
+		for (int i = 0; i < 4; i++)
+		{
+			std::cout << "mt amateria at idx:\t" << i << "\tw/addr:t" << mt.getKnownMaterias()[i] << std::endl;
+			std::cout << "mt_cp amateria at idx:\t" << i << "\tw/addr:t" << mt_cp.getKnownMaterias()[i] << std::endl;
+		}
+		delete ice;
+		delete cure;	
 	}
 
-	// {
-	// 	// Check for MateriaSource class
-	// 	std::cout << std::endl;
-	// 	std::cout << "/****** Provided main ******/" << std::endl;
+	{
+		// Check for MateriaSource class
+		std::cout << std::endl;
+		std::cout << "/****** Provided main ******/" << std::endl;
 		
-	// 	IMateriaSource *src = new MateriaSource();
-	// 	src->learnMateria(new Ice());
-	// 	src->learnMateria(new Cure());
-	// 	ICharacter *me = new Character("me");
-	// 	AMateria *tmp;
-	// 	tmp = src->createMateria("ice");
-	// 	me->equip(tmp);
-	// 	tmp = src->createMateria("cure");
-	// 	me->equip(tmp);
-	// 	ICharacter *bob = new Character("bob");
-	// 	me->use(0, *bob);
-	// 	me->use(1, *bob);
-	// 	delete bob;
-	// 	delete me;
-	// 	delete src;
-	// }
+		IMateriaSource *src = new MateriaSource();
+		src->learnMateria(new Ice()); //leak
+		src->learnMateria(new Cure()); //leak
+		ICharacter *me = new Character("me");
+		AMateria *tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		ICharacter *bob = new Character("bob");
+		me->use(0, *bob);
+		me->use(1, *bob);
+		delete bob;
+		delete me;
+		delete src;
+	}
 
 	return (0);
 }
